d2f9f8accbfe4c1b250951d643726c87330112b7
Dani <zsquiid@gmail.com>
2022-09-21 10:31:29 +0200

    enda en løsning

--- enkle_objekter.py
+++ enkle_objekter.py
@@ -1,16 +1,16 @@
-# rectangle = {
-#     "width": 3,
-#     "length": 5,
-#     "color": "blue"
-# }
+""" rectangle = {
+    "width": 3,
+    "length": 5,
+    "color": "blue"
+}
 
-# def rectangle_area(rectangle):
-#     area = rectangle["width"] * rectangle["length"]
-#     return area
+def rectangle_area(rectangle):
+    area = rectangle["width"] * rectangle["length"]
+    return area
 
-# rectangle["width"] = 10
-# print(rectangle["color"])
-# print(rectangle_area(rectangle))
+rectangle["width"] = 10
+print(rectangle["color"])
+print(rectangle_area(rectangle))  """
 
 
 class Rectangle:
@@ -31,3 +31,22 @@ rectangle.width = 4
 rectangle.length = 10
 print(rectangle.area())
 
+
+# class Rectangle:
+
+#     def __init__(self, width, length, color):
+
+#         self.width = width
+#         self.length = length
+#         self.color = color
+
+#     def area(self):
+#         area = self.width * self.length
+#         return area
+        
+
+# rectangle = Rectangle(3,5, "blue")
+
+# print(rectangle.color)
+
+# print(rectangle.area())
